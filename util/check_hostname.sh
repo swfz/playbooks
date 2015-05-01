f3fc886eb402dd61409b4c4b28bdd94f1375c9a0
swfz <sawafuji.09@gmail.com>
2015-05-01 19:55:31 +0900

    local sttings

--- util/check_hostname.sh
+++ util/check_hostname.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ip=`/sbin/ifconfig eth1 | grep -Po 'inet addr:\d+.\d+.\d+.\d+' | awk -F":" '{gsub(/\./, "-"); print $2}'`
-echo $ip
+echo -e "$ip\c"
 
---
+++ vagrant.local.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ssh-copy-id $1
+echo "vagrant ALL=(ALL) NOPASSWD: ALL"
+
+
+
+
